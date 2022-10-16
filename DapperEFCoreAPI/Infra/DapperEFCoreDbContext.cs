bfd0fb1520038c1a33daba7acc43f71db446d99f
Vinícius <vinigsouza.bnu@gmail.com>
2022-10-16 17:40:49 -0300

    feat: cria contexto e sessão do banco

---
+++ DapperEFCoreAPI/Infra/DapperEFCoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DapperEFCoreAPI.Infra
+{
+    public class DapperEFCoreDbContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public DapperEFCoreDbContext(IConfiguration configuration) : base() 
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
---
+++ DapperEFCoreAPI/Infra/DbSession.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DapperEFCoreAPI.Infra
+{
+    public class DbSession : IDisposable
+    {
+        public IDbConnection Connection { get; set; }
+
+        public DbSession(IConfiguration configuration)
+        {
+            Connection = new SqlConnection(configuration.GetConnectionString("DefaultConnection"));
+
+            Connection.Open();
+        }
+
+        public void Dispose() => Connection?.Dispose();
+    }
+}
