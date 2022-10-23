b42dd80a955d6b95534891c695a68aa511b4ca33
Vinícius <vinigsouza.bnu@gmail.com>
2022-10-23 18:21:28 -0300

    feat: implementa métodos de filtro de produto no serviço

--- DapperEFCoreAPI/Application/Interfaces/IProdutoAppService.cs
+++ DapperEFCoreAPI/Application/Interfaces/IProdutoAppService.cs
@@ -5,5 +5,18 @@ namespace DapperEFCoreAPI.Application.Interfaces
 {
     public interface IProdutoAppService : IServiceAppBase<Produto>
     {
+        /// <summary>
+        /// Método que retorna todos os produtos com a condição especificada de valor
+        /// </summary>
+        /// <param name="valor">Valor de comparação</param>
+        /// <param name="maiorQue">Caso true, retorna os itens maiores que o valor, se não, retorna os menores</param>
+        Task<List<Produto>> GetAllByValor(decimal valor, bool maiorQue);
+
+        /// <summary>
+        /// Método que retorna todos os produtos com a condição especificada de estoque
+        /// </summary>
+        /// <param name="estoque">Valor de estoque de comparação</param>
+        /// <param name="maiorQue">Caso true, retorna os itens com estoque maiores que o fornecido, se não, retorna os menores</param>
+        Task<List<Produto>> GetAllByEstoque(int estoque, bool maiorQue);
     }
 }
--- DapperEFCoreAPI/Application/Services/ProdutoAppService.cs
+++ DapperEFCoreAPI/Application/Services/ProdutoAppService.cs
@@ -9,5 +9,15 @@ namespace DapperEFCoreAPI.Application.Services
         public ProdutoAppService(IProdutoRepository repository) : base(repository)
         {
         }
+
+        public async Task<List<Produto>> GetAllByEstoque(int estoque, bool maiorQue)
+        {
+            return await ((IProdutoRepository)_repository).GetAllByEstoque(estoque, maiorQue);
+        }
+
+        public async Task<List<Produto>> GetAllByValor(decimal valor, bool maiorQue)
+        {
+            return await ((IProdutoRepository)_repository).GetAllByValor(valor, maiorQue);
+        }
     }
 }
