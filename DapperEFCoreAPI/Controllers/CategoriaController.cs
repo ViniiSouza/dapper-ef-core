c95403f93f6cf87993b5b6aa4525d75d7818595c
Vinícius <vinigsouza.bnu@gmail.com>
2022-10-23 18:47:36 -0300

    feat: implementa controller de categoria

---
+++ DapperEFCoreAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,48 @@
+﻿using DapperEFCoreAPI.Application.Interfaces;
+using DapperEFCoreAPI.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperEFCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/categoria")]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaAppService _categoriaAppService;
+
+        public CategoriaController(ICategoriaAppService categoriaAppService)
+        {
+            _categoriaAppService = categoriaAppService;
+        }
+
+        [HttpGet]
+        public async Task<List<Categoria>> GetAll()
+        {
+            return await _categoriaAppService.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Categoria> GetById([FromRoute] int id)
+        {
+            return await _categoriaAppService.GetByIdAsync(id);
+        }
+
+        [HttpPost]
+        public void Adicionar([FromBody] Categoria categoria)
+        {
+            _categoriaAppService.Add(categoria);
+        }
+
+        [HttpPut("{id}")]
+        public void Alterar([FromBody] Categoria categoria)
+        {
+            _categoriaAppService.Update(categoria);
+        }
+
+        [HttpDelete("{id}")]
+        public void Remover([FromRoute] int id)
+        {
+            _categoriaAppService.Delete(id);
+        }
+    }
+}
