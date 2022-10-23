afbff97e63ae792293c40303f942448b15c86060
Vinícius <vinigsouza.bnu@gmail.com>
2022-10-22 23:01:43 -0300

    feat: implementa interface e repositório genéricos

---
+++ DapperEFCoreAPI/Infra/Interfaces/IRepository.cs
@@ -0,0 +1,45 @@
+﻿namespace DapperEFCoreAPI.Infra.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Método de busca síncrono que retorna todos os registros da entidade
+        /// </summary>
+        List<TEntity> GetAll();
+
+        /// <summary>
+        /// Método de busca assíncrono que retorna todos os registros da entidade
+        /// </summary>
+        Task<List<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// Método de busca síncrono que retorna a entidade do id informado
+        /// </summary>
+        /// <param name="id">Id da entidade desejada</param>
+        TEntity GetById(int id);
+
+        /// <summary>
+        /// Método de busca assíncrono que retorna a entidade do id informado
+        /// </summary>
+        /// <param name="id">Id da entidade desejada</param>
+        Task<TEntity> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Método de adição de entidade no banco
+        /// </summary>
+        /// <param name="entity">Entidade a ser adicionada</param>
+        void Add(TEntity entity);
+
+        /// <summary>
+        /// Método de atualização de entidade no banco
+        /// </summary>
+        /// <param name="entity">Entidade a ser atualizada</param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// Método de deleção de entidade do banco
+        /// </summary>
+        /// <param name="id">Id da entidade a ser deletada</param>
+        void Delete(int id);
+    }
+}
---
+++ DapperEFCoreAPI/Infra/Repositories/Repository.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using DapperEFCoreAPI.Infra.Interfaces;
+
+namespace DapperEFCoreAPI.Infra.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private DbSession _session;
+        private DapperEFCoreDbContext _dbContext;
+
+        public Repository(DbSession session)
+        {
+            _session = session;
+        }
+
+        public List<TEntity> GetAll()
+        {
+            using (var connection = _session.Connection)
+            {
+                string query = $"SELECT * FROM {typeof(TEntity).Name}";
+                List<TEntity> itens = connection.Query<TEntity>(query).ToList();
+                return itens;
+            }
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            using (var connection = _session.Connection)
+            {
+                string query = $"SELECT * FROM {typeof(TEntity).Name}";
+                List<TEntity> itens = (await connection.QueryAsync<TEntity>(query)).ToList();
+                return itens;
+            }
+        }
+
+        public TEntity GetById(int id)
+        {
+            using (var connection = _session.Connection)
+            {
+                string query = $"SELECT * FROM {typeof(TEntity).Name} WHERE Id = @id";
+                TEntity entity = connection.QueryFirstOrDefault<TEntity>(query, new { id });
+
+                return entity;
+            }
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            using (var connection = _session.Connection)
+            {
+                string query = $"SELECT * FROM {typeof(TEntity).Name} WHERE Id = @id";
+                TEntity entity = await connection.QueryFirstOrDefaultAsync<TEntity>(query, new { id });
+
+                return entity;
+            }
+        }
+
+        public void Add(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var entity = GetById(id);
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+    }
+}
