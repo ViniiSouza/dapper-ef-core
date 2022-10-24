8e8f8528bca33dd8fcdb7dd02833a590097aa8b5
Vinícius <vinigsouza.bnu@gmail.com>
2022-10-23 23:17:53 -0300

    feat: criação e implementação do resolvedor de dependências

---
+++ DapperEFCoreAPI/Security/DependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿using DapperEFCoreAPI.Application.Interfaces;
+using DapperEFCoreAPI.Application.Services;
+using DapperEFCoreAPI.Infra;
+using DapperEFCoreAPI.Infra.Interfaces;
+using DapperEFCoreAPI.Infra.Repositories;
+
+namespace DapperEFCoreAPI.Security
+{
+    public static class DependencyResolver
+    {
+        public static void AddDependencies(this IServiceCollection service)
+        {
+            service.AddScoped<DapperEFCoreDbContext>();
+            service.AddScoped<IUnitOfWork, UnitOfWork>();
+            service.AddScoped<DbSession>();
+            service.AddTransient<ICategoriaRepository, CategoriaRepository>();
+            service.AddTransient<IProdutoRepository, ProdutoRepository>();
+            service.AddTransient<ICategoriaAppService, CategoriaAppService>();
+            service.AddTransient<IProdutoAppService, ProdutoAppService>();
+        }
+    }
+}
