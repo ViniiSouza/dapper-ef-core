446dded34cab4bdbee554f0742a3746a15c22317
Vinícius <vinigsouza.bnu@gmail.com>
2022-10-23 22:26:02 -0300

    feat: implementa controller de produto

---
+++ DapperEFCoreAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,66 @@
+﻿using DapperEFCoreAPI.Application.Interfaces;
+using DapperEFCoreAPI.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperEFCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("api/produto")]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoAppService _produtoAppService;
+
+        public ProdutoController(IProdutoAppService produtoAppService)
+        {
+            _produtoAppService = produtoAppService;
+        }
+
+        [HttpGet]
+        public async Task<List<Produto>> GetAll()
+        {
+            return await _produtoAppService.GetAllAsync();
+        }
+
+        [HttpGet("estoque")]
+        public async Task<List<Produto>> GetAllByEstoque([FromQuery] int estoque, [FromQuery] bool maiorQue)
+        {
+            return await _produtoAppService.GetAllByEstoque(estoque, maiorQue);
+        }
+
+        [HttpGet("valor")]
+        public async Task<List<Produto>> GetAllByValor([FromQuery] decimal valor, [FromQuery] bool maiorQue)
+        {
+            return await _produtoAppService.GetAllByValor(valor, maiorQue);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Produto> GetById([FromRoute] int id)
+        {
+            return await _produtoAppService.GetByIdAsync(id);
+        }
+
+        [HttpGet("categoria/{categoriaId}")]
+        public async Task<List<Produto>> GetAllByCategoria([FromRoute] int categoriaId)
+        {
+            return await _produtoAppService.GetAllByCategoria(categoriaId);
+        }
+
+        [HttpPost]
+        public void Adicionar([FromBody] Produto categoria)
+        {
+            _produtoAppService.Add(categoria);
+        }
+
+        [HttpPut("{id}")]
+        public void Alterar([FromBody] Produto categoria)
+        {
+            _produtoAppService.Update(categoria);
+        }
+
+        [HttpDelete("{id}")]
+        public void Remover([FromRoute] int id)
+        {
+            _produtoAppService.Delete(id);
+        }
+    }
+}
